4cbdba6ecb2fcf30158b3d20d710cb91379cbf9e
SilverGlobe Solutions <bhavinathunt@gmail.com>
2017-10-25 22:08:40 +0530

    Library upload

---
+++ src/main/java/com/cazzy/cazzysimplenetworking/Networking/ResponseHandler.java
@@ -0,0 +1,12 @@
+package com.cazzy.cazzysimplenetworking.Networking;
+
+/**
+ * Created by silverglobesolutions on 25/10/17.
+ */
+
+public interface ResponseHandler {
+
+    public void getResponse(ServerResponseWrapper serverResponseWrapper);
+
+
+}
